97eee713f62ee576335f17e87504e44bee2af9e7
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-07-25 22:30:24 +0530

    Homepage: Added Dialog

--- src/components/ImageResult.js
+++ src/components/ImageResult.js
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GridList,
   GridListTile,
   GridListTileBar,
-  IconButton
+  IconButton,
+  Dialog
 } from "@material-ui/core";
 import { ZoomIn } from "@material-ui/icons";
 
 export default function ImageResult(props) {
   const { images } = props;
 
+  const [open, setOpen] = useState(false);
+  const [currentImg, setCurrentImage] = useState("");
+
+  function handleOpen(img) {
+    setOpen(true);
+    setCurrentImage(img);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
-    <GridList cols={3}>
-      {images.map(img => (
-        <GridListTile key={img.id}>
-          <img src={img.largeImageURL} />
-          <GridListTileBar
-            title={img.tags}
-            subtitle={
-              <span>
-                by <strong>{img.user}</strong>
-              </span>
-            }
-            actionIcon={
-              <IconButton>
-                <ZoomIn style={{ color: "white" }} />
-              </IconButton>
-            }
-          />
-        </GridListTile>
-      ))}
-    </GridList>
+    <React.Fragment>
+      <GridList cols={3}>
+        {images.map(img => (
+          <GridListTile key={img.id}>
+            <img src={img.largeImageURL} alt="img" />
+            <GridListTileBar
+              title={img.tags}
+              subtitle={
+                <span>
+                  by <strong>{img.user}</strong>
+                </span>
+              }
+              actionIcon={
+                <IconButton onClick={() => handleOpen(img.largeImageURL)}>
+                  <ZoomIn style={{ color: "white" }} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+      <Dialog onClose={handleClose} open={open}>
+        <img src={currentImg} alt="current-img" style={{ width: "100%" }} />
+      </Dialog>
+    </React.Fragment>
   );
 }
