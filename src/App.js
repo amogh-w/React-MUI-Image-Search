47e4e25e63255e67309fbf3a5045cb9fc7f43ce6
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-07-25 15:12:35 +0530

    Homepage: Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Paper } from "@material-ui/core";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <p>App</p>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Paper square={true} style={{ marginTop: "20px" }}>
+          <Search />
+        </Paper>
+      </React.Fragment>
     );
   }
 }
